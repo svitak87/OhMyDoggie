e6fc286fee201bb1ae8cf06a3a89039c092f5e49
OhMyDoggie <zasuanimalscaja@gmail.com>
2024-12-05 00:16:02 -0300

    testing the backend

--- backend/database.js
+++ backend/database.js
@@ -4,6 +4,7 @@ const defineAppointment = require("./models/appointment");
 const defineAdmin = require("./models/adminModel")
 
 const isProduction = process.env.NODE_ENV === 'production';
+console.log("Database URL:", process.env.DATA_BASE_PUBLIC);
 
 const sequelize = new Sequelize(process.env.DATA_BASE_PUBLIC, {
   logging: false,
