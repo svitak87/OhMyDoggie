339568341fffd085e891efa2f4782f717457941e
OhMyDoggie <zasuanimalscaja@gmail.com>
2024-12-05 01:19:08 -0300

    solving pg

--- backend/database.js
+++ backend/database.js
@@ -6,6 +6,9 @@ const defineAdmin = require("./models/adminModel")
 const isProduction = process.env.NODE_ENV === 'production';
 console.log("Database URL:", process.env.DATA_BASE_PUBLIC);
 
+
+
+
 const sequelize = new Sequelize(process.env.DATA_BASE_PUBLIC, {
   logging: false,
   dialectOptions: isProduction
